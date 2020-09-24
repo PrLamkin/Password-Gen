b1fe49dd4b3cb48baee8a64ea33f3e9efc4d1510
PrLamkin <shiektheimmortal@outlook.com>
2020-09-24 18:22:40 -0500

    Updated HTML File to my preferences-2

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
